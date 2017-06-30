bc254da8dd5e0714b2961e2b607a16a76c7c6297
miky <mathias89100@gmail.com>
2017-06-30 09:31:34 +0200

    All

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -25,9 +25,6 @@ class Configuration implements ConfigurationInterface
             ->scalarNode('user_class')->isRequired()->cannotBeEmpty()->end()
             ->scalarNode('employee_class')->isRequired()->cannotBeEmpty()->end()
             ->end();
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         return $treeBuilder;
     }
