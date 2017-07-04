81950fd4baf178d7f7ae7d44e5010180a852c016
miky <mathias89100@gmail.com>
2017-07-04 10:40:12 +0200

    Merge branch 'master' of github.com:mikyprog/Order
    # Conflicts:
    #	.gitignore

--- Controller/Admin/CustomerAdminController.php
+++ Controller/Admin/CustomerAdminController.php
@@ -9,22 +9,7 @@
 namespace Miky\Bundle\UserBundle\Controller\Admin;
 
 
-use Doctrine\Common\Persistence\ObjectManager;
-use Miky\Bundle\ResourceBundle\Controller\AuthorizationCheckerInterface;
-use Miky\Bundle\ResourceBundle\Controller\EventDispatcherInterface;
-use Miky\Bundle\ResourceBundle\Controller\FlashHelperInterface;
-use Miky\Bundle\ResourceBundle\Controller\NewResourceFactoryInterface;
-use Miky\Bundle\ResourceBundle\Controller\RedirectHandlerInterface;
-use Miky\Bundle\ResourceBundle\Controller\RequestConfigurationFactoryInterface;
 use Miky\Bundle\ResourceBundle\Controller\ResourceController;
-use Miky\Bundle\ResourceBundle\Controller\ResourceFormFactoryInterface;
-use Miky\Bundle\ResourceBundle\Controller\ResourcesCollectionProviderInterface;
-use Miky\Bundle\ResourceBundle\Controller\SingleResourceProviderInterface;
-use Miky\Bundle\ResourceBundle\Controller\StateMachineInterface;
-use Miky\Bundle\ResourceBundle\Controller\ViewHandlerInterface;
-use Miky\Component\Resource\Factory\FactoryInterface;
-use Miky\Component\Resource\Metadata\MetadataInterface;
-use Miky\Component\Resource\Repository\RepositoryInterface;
 
 class CustomerAdminController extends ResourceController
 {
