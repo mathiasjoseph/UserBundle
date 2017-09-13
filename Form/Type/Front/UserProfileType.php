7b18c6069d58e84c543dcb06b0a6fe8b9f0f0f12
miky <mathias89100@gmail.com>
2017-09-13 06:35:17 +0200

    Merge branch 'master' of github.com:mikyprog/Order
    # Conflicts:
    #	.gitignore

---
+++ Form/Type/Front/UserProfileType.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 09/08/16
+ * Time: 09:40
+ */
+
+namespace Miky\Bundle\UserBundle\Form\Type\Front;
+
+
+use Miky\Bundle\LocaleBundle\Manager\LanguageManager;
+use Miky\Bundle\LocationBundle\Form\Type\Frontend\LocationType;
+use Miky\Bundle\LocationBundle\Form\Type\GoogleGeocodingType;
+use Miky\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserProfileType extends AbstractResourceType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', TextType::class)
+            ->add('username', TextType::class)
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+                "required" => false
+            ))
+            ->add('profileImage', 'miky_media_type', array(
+                'provider' => 'miky.media.provider.image',
+                'context'  => 'customer'
+            ))
+        ;
+    }
+}
