62b726b19c0643b75a36c706d476c44738bc3f86
miky <mathias89100@gmail.com>
2017-09-28 04:15:13 +0200

    Merge branch 'master' of github.com:mikyprog/Order
    # Conflicts:
    #	.gitignore

--- Doctrine/EmployeeManager.php
+++ Doctrine/EmployeeManager.php
@@ -13,12 +13,36 @@ use Doctrine\Common\Persistence\ObjectManager;
 use FOS\UserBundle\Doctrine\UserManager;
 use FOS\UserBundle\Util\CanonicalFieldsUpdater;
 use FOS\UserBundle\Util\PasswordUpdaterInterface;
+use Miky\Bundle\UserBundle\Model\Employee;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class EmployeeManager extends UserManager
 {
+    /**
+     * @var TokenStorageInterface
+     */
+    protected $tokenStorage;
 
-    public function __construct(PasswordUpdaterInterface $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater, ObjectManager $om, $class)
+    public function __construct(PasswordUpdaterInterface $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater, ObjectManager $om, TokenStorageInterface $tokenStorage, $class)
     {
         parent::__construct($passwordUpdater, $canonicalFieldsUpdater, $om, $class);
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public function getCurrentUser()
+    {
+        if (null === $token = $this->tokenStorage->getToken()) {
+            return;
+        }
+
+        if (!is_object($user = $token->getUser())) {
+            // e.g. anonymous authentication
+            return;
+        }
+
+        if (!$user instanceof Employee) {
+            return;
+        }
+        return $user;
     }
 }
