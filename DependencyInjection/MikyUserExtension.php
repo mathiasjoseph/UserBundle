fa667121954644f235619ae9d4dc84740e91a5a8
miky <mathias89100@gmail.com>
2017-09-30 02:18:24 +0200

    Merge branch 'master' of github.com:mikyprog/Order
    # Conflicts:
    #	.gitignore

--- DependencyInjection/MikyUserExtension.php
+++ DependencyInjection/MikyUserExtension.php
@@ -22,7 +22,7 @@ class MikyUserExtension extends AbstractCoreExtension implements PrependExtensio
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
+        $this->addBundleRequired("FOSUserBundle", $container);
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
